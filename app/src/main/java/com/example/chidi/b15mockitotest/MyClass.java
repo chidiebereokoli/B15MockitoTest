f836e48660b935b7732f6ce35954d0043a8e7b13
chidiebereokoli <chidiebereokoli@gmail.com>
2018-10-09 14:42:31 -0500

    Spy object test using mockito

--- app/src/main/java/com/example/chidi/b15mockitotest/MyClass.java
+++ app/src/main/java/com/example/chidi/b15mockitotest/MyClass.java
@@ -4,4 +4,10 @@ class MyClass {
     public int getUniqueId() {
         return 0;
     }
+
+    public void testing(int i) {
+    }
+
+    public void someMethod(String never_called) {
+    }
 }
--- app/src/test/java/com/example/chidi/b15mockitotest/ExampleUnitTest.java
+++ app/src/test/java/com/example/chidi/b15mockitotest/ExampleUnitTest.java
@@ -1,14 +1,19 @@
 package com.example.chidi.b15mockitotest;
+
 import org.junit.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
 
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
 
-public class ExampleUnitTest{
-@Test
-public void test1()  {
+public class ExampleUnitTest {
+    @Test
+    public void test1() {
         //  create mock
         MyClass test = mock(MyClass.class);
 
@@ -17,47 +22,102 @@ public void test1()  {
 
         // use mock in test....
         assertEquals(test.getUniqueId(), 43);
-        }
+    }
 
 
-// demonstrates the return of multiple values
-@Test
-public void testMoreThanOneReturnValue()  {
-        Iterator<String> i= mock(Iterator.class);
+    // demonstrates the return of multiple values
+    @Test
+    public void testMoreThanOneReturnValue() {
+        Iterator<String> i = mock(Iterator.class);
         when(i.next()).thenReturn("Mockito").thenReturn("rocks");
-        String result= i.next()+" "+i.next();
+        String result = i.next() + " " + i.next();
         //assert
         assertEquals("Mockito rocks", result);
-        }
+    }
 
-// this test demonstrates how to return values based on the input
-@Test
-public void testReturnValueDependentOnMethodParameter()  {
-        Comparable<String> c= mock(Comparable.class);
+    // this test demonstrates how to return values based on the input
+    @Test
+    public void testReturnValueDependentOnMethodParameter() {
+        Comparable<String> c = mock(Comparable.class);
         when(c.compareTo("Mockito")).thenReturn(1);
         when(c.compareTo("Eclipse")).thenReturn(2);
         //assert
         assertEquals(1, c.compareTo("Mockito"));
-        }
+    }
 
 // this test demonstrates how to return values independent of the input value
 
-@Test
-public void testReturnValueInDependentOnMethodParameter()  {
-        Comparable<Integer> c= mock(Comparable.class);
+    @Test
+    public void testReturnValueInDependentOnMethodParameter() {
+        Comparable<Integer> c = mock(Comparable.class);
         when(c.compareTo(anyInt())).thenReturn(-1);
         //assert
         assertEquals(-1, c.compareTo(9));
-        }
+    }
 
 // return a value based on the type of the provide parameter
 
-@Test
-public void testReturnValueInDependentOnMethodParameter2()  {
-        Comparable<Todo> c= mock(Comparable.class);
+    @Test
+    public void testReturnValueInDependentOnMethodParameter2() {
+        Comparable<Todo> c = mock(Comparable.class);
         when(c.compareTo(isA(Todo.class))).thenReturn(0);
         //assert
         assertEquals(0, c.compareTo(new Todo(1)));
-        }
+    }
 
+    @Test
+    public void testLinkedListSpyWrong() {
+        // Lets mock a LinkedList
+        List<String> list = new LinkedList<>();
+        List<String> spy = spy(list);
+
+        // this does not work
+        // real method is called so spy.get(0)
+        // throws IndexOutOfBoundsException (list is still empty)
+        when(spy.get(0)).thenReturn("foo");
+
+        assertEquals("foo", spy.get(0));
+    }
+
+    @Test
+    public void testLinkedListSpyCorrect() {
+        // Lets mock a LinkedList
+        List<String> list = new LinkedList<>();
+        List<String> spy = spy(list);
+
+        // You have to use doReturn() for stubbing
+        doReturn("foo").when(spy).get(0);
+
+        assertEquals("foo", spy.get(0));
+    }
+
+    @Test
+    public void testVerify()  {
+        // create and configure mock
+        MyClass test = Mockito.mock(MyClass.class);
+        when(test.getUniqueId()).thenReturn(43);
+
+
+        // call method testing on the mock with parameter 12
+        test.testing(12);
+        test.getUniqueId();
+        test.getUniqueId();
+
+
+        // now check if method testing was called with the parameter 12
+        //verify(test).testing(ArgumentMatchers.eq(13));
+
+        // was the method called twice?
+        verify(test, times(3)).getUniqueId();
+/*
+        // other alternatives for verifiying the number of method calls for a method
+        verify(test, never()).someMethod("never called");
+        verify(test, atLeastOnce()).someMethod("called at least once");
+        verify(test, atLeast(2)).someMethod("called at least twice");
+        verify(test, times(5)).someMethod("called five times");
+        verify(test, atMost(3)).someMethod("called at most 3 times");
+        // This let's you check that no other methods where called on this object.
+        // You call it after you have verified the expected method calls.
+        verifyNoMoreInteractions(test);*/
+    }
 }
